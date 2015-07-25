6bc2c9e3ffdb3f52af6393616d8d60e6a4f4411f
league-workshopper <workshop@AdministorsMini.attlocal.net>
2015-07-25 16:04:12 -0700

    Finished Robot version of Pentagon Crazy.

--- src/day1/robot/PentagonCrazy.java
+++ src/day1/robot/PentagonCrazy.java
@@ -1,4 +1,4 @@
-package solutions;
+package day1.robot;
 
 import java.awt.Color;
 
@@ -6,40 +6,39 @@ import org.jointheleague.graphical.robot.Robot;
 
 /* Teacher’s note: before beginning, draw a pentagon and have students work out the angle that the robot will have to turn (360/5) */
 
-public class PentagonCrazySolution {
+public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
-		Robot robot = new Robot();
+
 		// 3. Put the robot's pen down
-		robot.penDown();
+
 		// 8. Make the robot go at maximum speed (10)
-		robot.setSpeed(10);
+
 		// 9. Set the pen to a color that you like for the shape
-		robot.setPenColor(Color.CYAN);
+
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-		int sides = 5;
+
 		// 5. Make a variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6.
-		int angle = 360 / sides;
-		// 7. Do everything below 200 times. When this is done you should see a pentagon.
-		for (int i = 0; i < 200; i++) {
+
+		// 7. Do steps #2 to #11, 200 times. When this is done you should see a pentagon.
+
 			// 2. Move the robot 200 pixels
-			// robot.move(200);
-			// 9. Make the robot move "i" pixels instead of 200 (don’t need new line of code for this, just change previous one)
-			robot.move(i);
+	
+			// 10. Make the robot move "i" pixels instead of 200 (don’t need new line of code for this, just change previous one)
+	
 			// 6. Turn the robot the amount in your angle variable
-			robot.turn(angle);
-			// 10. Turn the robot one more degree
-			robot.turn(1);
-		}
+	
+			// 11. Turn the robot one more degree
+
 	}
 
 	// Variations:
-	// *11. make the pattern really huge
-	// *12. change the color of the pattern
-	// *13. experiment with different shapes
+	// *12. make the pattern really huge
+	// *13. randomize the color of the pattern 
+	// *14. experiment with different shapes
 
 	public static void main(String[] args) {
-		new PentagonCrazySolution().makePrettyThings();
+		new PentagonCrazy().makePrettyThings();
 	}
 }
