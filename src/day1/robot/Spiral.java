5c1fceb003f6ea134ceb79e4e034fd8d34f293c3
league-workshopper <workshop@AdministorsMini.attlocal.net>
2015-07-25 16:14:34 -0700

    Fixed indentation.

--- src/day1/robot/Spiral.java
+++ src/day1/robot/Spiral.java
@@ -9,15 +9,15 @@ public class Spiral {
 
 		// 4. Do the following (steps 5-8) 75 times
 
-		// 6. Change the pen color to random
-
-		// 5. Move the robot 5 times the current line number you are drawing (5*i)
-
-		// 2. Turn the robot 1/3 of 360 degrees to the right
-
-		// 7. Change the number of sides to 7 (don’t add a new line of code for this one!)
-
-		// 8. Set the pen width to i
+			// 6. Change the pen color to random
+	
+			// 5. Move the robot 5 times the current line number you are drawing (5*i)
+	
+			// 2. Turn the robot 1/3 of 360 degrees to the right
+	
+			// 7. Change the number of sides to 7 (don’t add a new line of code for this one!)
+	
+			// 8. Set the pen width to i
 
 	}
 }
