b687c03dec5e709c4a57171d514c5abff8dae7e8
league-workshopper-4 <workshop@Mac-mini.local>
2015-07-27 15:36:13 -0700

    new version flaming ninja star

---
+++ src/FlamingNinjaStar.java
@@ -0,0 +1,48 @@
+import org.jointheleague.graphical.robot.Robot;
+
+/*** Teacher's note ***/
+ /* Before beginning recipe: 
+ * 1. ask students to find and explain the variable in this recipe. 
+ * 2. ask students what robot commands they think they will use to make picture in the laminated hand-outs. */
+
+public class FlamingNinjaStar {
+	public static void main(String[] args) {
+
+		int baseSize = 300;		//the size of the black part of the star
+		int flameSize = 200;		//the length of the flaming arms
+
+		// *14. Use the methods setX and setY to move the ninja star into the center of the screen
+		
+		// *15. Make some adjustments to see what other kinds of shapes you can make.
+
+		// 1. Show the robot
+
+		// 12. Set the robot speed to 10
+
+		// 13. Make all the code below repeat 25 times
+
+			// 2. Turn the robot 1/8 of a circle
+		
+			// 3. Move the robot 64 pixels
+
+			
+			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
+			
+			// 5. Move the robot the distance in the variable flameSize
+			
+			// 6. Turn the robot 170 degrees
+			
+			// 7. Move the robot the distance in the variable flameSize (again)
+			
+			// 8. Turn the robot 64 degrees to the right
+			
+			// 9. Move the robot the distance in the variable baseSize
+			
+			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
+			// 11. Color your ninja star like Figure 2.
+		
+	}
+
+}
+
+
