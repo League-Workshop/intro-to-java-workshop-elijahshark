a83c300e2fd86f3d534292fd7d46593b69cb3282
June Clarke <joonspoon@joonspoon.com>
2015-07-27 17:37:30 -0700

    fixed numbering and added import

--- src/day1/robot/FourSquare.java
+++ src/day1/robot/FourSquare.java
@@ -1,28 +1,29 @@
 package day1.robot;
 
 import javax.swing.JOptionPane;
+import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
 	
-	// 1. Create a new Robot
+	// 2. Create a new Robot
 	
 
 	void go() {
-		// 3. Make the robot move as fast as possible
+		// 4. Make the robot move as fast as possible
 
-		// 4. Set the pen width to 5
+		// 5. Set the pen width to 5
 
-		// 5. Do steps #6 to #7 four times...
+		// 6. Do steps #6 to #7 four times...
 
-			// 6. Set the pen color to random
+			// 7. Set the pen color to random
 	
 			// 1. Call the drawSquare() method
 	
-			// 7. Turn the robot 90 degrees to the right
+			// 8. Turn the robot 90 degrees to the right
 
 	}
 
-	/* 2. Fill in the code to draw a square inside the method below. */
+	/* 3. Fill in the code to draw a square inside the method below. */
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 
