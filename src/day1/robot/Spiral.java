607347adf7e61d50d3621181c432db66c31df74c
June Clarke <The League of Amazing Programmers>
2015-08-03 10:41:30 -0700

    added Robot import and require penDown command

--- src/day1/robot/Spiral.java
+++ src/day1/robot/Spiral.java
@@ -1,23 +1,27 @@
 package day1.robot;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Spiral {
 
 	public static void main(String[] args) {
-		// 1. Create a new Robot and set its pen to the down position
+		// 1. Create a new Robot
+		
+		// 5. Set your robot's pen to the down position
 
 		// 3. Set the robot to go at max speed (10)
 
 		// 4. Do the following (steps 5-8) 75 times
 
-			// 6. Change the pen color to random
+			// 7. Change the pen color to random
 	
-			// 5. Move the robot 5 times the current line number you are drawing (5*i)
+			// 6. Move the robot 5 times the current line number you are drawing (5*i)
 	
 			// 2. Turn the robot 1/3 of 360 degrees to the right
 	
-			// 7. Change the number of sides to 7 (don’t add a new line of code for this one!)
+			// 8. Change the number of sides to 7 (don’t add a new line of code for this one!)
 	
-			// 8. Set the pen width to i
+			// 9. Set the pen width to i
 
 	}
 }
