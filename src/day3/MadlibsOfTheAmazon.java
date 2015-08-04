d0e165b47170ec518e3acb6c38c8c8d7305395af
June Clarke <The League of Amazing Programmers>
2015-08-04 15:17:51 -0700

    Added Madlibs to day3

---
+++ src/day3/MadlibsOfTheAmazon.java
@@ -0,0 +1,33 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class MadlibsOfTheAmazon {
+	public static void main(String[] args) {
+
+		// Put this sentence in a pop up:
+		// If you find yourself having to cross a piranha-infested river, here's how to do it...
+
+		// Get the user to enter an adjective
+
+		// Get the user to enter a type of liquid
+
+		// Get the user to enter a body part
+
+		// Get the user to enter a verb
+
+		// Get the user to enter a place
+
+		// Fit the user's words into this sentence, and save it in a String:
+		// Piranhas are more [adjective] during the day, so cross the river at
+		// night. Piranhas are attracted to fresh [type of liquid] and will most
+		// likely take a bite out of your [body part] if you [verb]. Whatever
+		// you do, if you have an open wound, try to find another way to get
+		// back to the [place]. Good luck!
+		
+		// Make a pop-up for the final story. You can use \n to go to the next line.
+		
+
+	}
+}
+
